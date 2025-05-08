70488995cab52bda5fe7bb4165aae07d30991336
renatomarf76 <renatomarf76@egmail.com>
2025-05-07 22:54:23 -0300

    16 - Tenant Pages

---
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/page.tsx
@@ -0,0 +1,33 @@
+import type { SearchParams } from "nuqs/server";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { loadProductFilters } from "@/modules/products/search-params";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { DEFAULT_LIMIT } from "@/constants";
+
+interface Props {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<SearchParams>;
+}
+
+const Page = async ({ params, searchParams }: Props) => {
+  const { slug } = await params;
+  const filters = await  loadProductFilters(searchParams);
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchInfiniteQuery(trpc.products.getMany.infiniteQueryOptions({
+    ...filters,
+    tenantSlug: slug,
+    limit: DEFAULT_LIMIT,
+  }));
+
+  return ( 
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductListView tenantSlug={slug} narrowView />
+    </HydrationBoundary>
+   );
+};
+ 
+export default Page;
