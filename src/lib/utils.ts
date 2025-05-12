5f3bd0d11ea54ad1f8fbdb8ea93d951c6a87cdd4
renatomarf76 <renatomarf76@egmail.com>
2025-05-12 17:43:15 -0300

    28 - Deployment Fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,20 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantURL(tenantSlug: string) {
-  return `/tenants/${tenantSlug}`;
-}
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!);
+
+  // In development or subdomain routing disabled mode, use normal routing
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
+    return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
+  }
+
+  const protocol = "https";
+  const domain= process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
+
+  // In production, use subdomain routing
+  return `${protocol}://${tenantSlug}.${domain}`;
+};
 
 export function formatCurrency(value: number | string) {
   return new Intl.NumberFormat("en-US", {
