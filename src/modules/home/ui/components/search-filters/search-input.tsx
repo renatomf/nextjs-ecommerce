ad4e9d2cf97f7f46f4a09d4755a484fba362ea76
renatomarf76 <renatomarf76@egmail.com>
2025-05-13 00:33:52 -0300

    32 - Search Filter

--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
 import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
+import { useTRPC } from "@/trpc/client";
 
 import { Input } from "@/components/ui/input";
 import { CategoriesSidebar } from "./categories-sidebar";
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-import Link from "next/link";
+import { useProductFilters } from "@/modules/products/hooks/use-product-filters";
 
 interface Props {
   disabled?: boolean;
@@ -17,17 +18,33 @@ interface Props {
 export const SearchInput = ({ 
   disabled, 
 }: Props) => {
+  const [filters, setFilters] = useProductFilters();
+  const [searchValue, setSearchValue] = useState(filters.search);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const trpc = useTRPC();
   const session = useQuery(trpc.auth.session.queryOptions());
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setFilters({ search: searchValue });
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchValue, setFilters]);
 
   return ( 
     <div className="flex items-center gap-2 w-full">
       <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
       <div className="relative w-full">
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
-        <Input className="pl-8" placeholder="Search products" disabled={disabled}  />
+        <Input 
+          className="pl-8"
+          placeholder="Search products"
+          disabled={disabled}
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+         />
       </div>
       <Button
         variant="elevated"
